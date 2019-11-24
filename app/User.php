e001fdbf779d0a0e474db610da4d0506c205e21f
Stefan Zweifel <hello@stefanzweifel.io>
2019-11-24 12:22:54 +0100

    Trigger

--- app/User.php
+++ app/User.php
@@ -2,8 +2,11 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
