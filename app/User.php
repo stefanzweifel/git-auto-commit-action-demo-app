e7cf80a6b47008e19c4a6e9d9438e58107b6edfa
Stefan Zweifel <hello@stefanzweifel.io>
2020-04-18 09:53:12 +0200

    Trigger

--- app/User.php
+++ app/User.php
@@ -14,7 +14,8 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
+    protected $fillable =
+    [
         'name', 'email', 'password',
     ];
 
