ae421c41742ecd7d7a1b1ce14428b824964553f1
Stefan Zweifel <stefanzweifel@users.noreply.github.com>
2020-08-21 20:48:35 +0200

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
