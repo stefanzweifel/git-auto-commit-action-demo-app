ed7a4b344a580eef0e76728b3b02178ebf1bb2ec
stefanzweifel <stefanzweifel@users.noreply.github.com>
2022-09-24 09:06:29 +0000

    Apply automatic changes

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -11,7 +11,8 @@ trait CreatesApplication
      *
      * @return \Illuminate\Foundation\Application
      */
-    public function createApplication() {
+    public function createApplication()
+    {
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
