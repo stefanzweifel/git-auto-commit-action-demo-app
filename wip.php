2079187547f1490b255280ccb1382ae5e2aa2895
Stefan Zweifel <hello@stefanzweifel.io>
2022-10-22 20:35:07 +0200

    QIP

--- wip.php
+++ wip.php
@@ -9,6 +9,11 @@ $value = 1;
 
 $gitHubOutput .= "$key=$value\n";
 
+$key = 'release_compare_url';
+$value = 'https://github.com/org/repo/compare/v1.0.0...v1.1.0';
+
+$gitHubOutput .= "$key=$value\n";
+
 // putenv("GITHUB_OUTPUT=$gitHubOutput");
 
 file_put_contents($pathToGitHubOutput, $gitHubOutput, FILE_APPEND | LOCK_EX);
