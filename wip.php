cb51037b77b4e2858172fd4d7103e30de23133a7
Stefan Zweifel <hello@stefanzweifel.io>
2022-10-22 20:28:55 +0200

    WIP

---
+++ wip.php
@@ -0,0 +1,12 @@
+<?php 
+
+$gitHubOutput = getenv('GITHUB_OUTPUT');
+
+echo $gitHubOutput;
+
+$key = 'changes_detected';
+$value = 1;
+
+// $gitHubOutput .= "$key=$value";
+
+// putenv("GITHUB_OUTPUT=$gitHubOutput");
