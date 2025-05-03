42c2ae1f8d1b5d5d487c2530983ac0b67bc19c92
stefanzweifel <1080923+stefanzweifel@users.noreply.github.com>
2025-05-03 07:43:22 +0000

    Commit Message 1 Commit Message 2 Commit Message 3

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
